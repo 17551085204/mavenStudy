1ec0dea290b3018a27cb0969a8099fda89508bfc
hdk <2890241339@qq.com>
2021-02-07 16:56:13 +0800

    1
    1

--- springboot-quick/src/main/java/com/example/config/MyAppConfig.java
+++ springboot-quick/src/main/java/com/example/config/MyAppConfig.java
@@ -15,6 +15,7 @@ public class MyAppConfig {
 
     @Bean //将方法的返回值添加到容器中：默认id就是方法名
     public HelloService helloService() {
+        System.out.println("推荐使用配置类给容器中加组件");
         return new HelloService();
     }
 }
--- springboot-quick/src/test/java/com/example/SpringbootQuickApplicationTests.java
+++ springboot-quick/src/test/java/com/example/SpringbootQuickApplicationTests.java
@@ -1,20 +1,35 @@
 package com.example;
 
 import com.example.bean.Person;
+//import org.apache.catalina.core.ApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class SpringbootQuickApplicationTests {
 
+
+
+    @Autowired
+    ApplicationContext ioc;
+    @Test
+    public void testHelloService(){
+        System.out.println(ioc.containsBean("helloService"));
+    }
+
+
     @Autowired
     Person person;
-
     @Test
     void contextLoads() {
         System.out.println(person);
         System.out.println(person.getLastName());
     }
+
+
+
 
 }
